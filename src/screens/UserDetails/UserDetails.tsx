9cdcee4df9ed8b7ffed77dcbd95cb54814307e05
PVMGusmao <paulo.gusmao@stellabarros.com.br>
2023-12-09 01:59:48 -0300

    preenchida com as informações de usuário a tela de detalhes de usuário

--- src/screens/UserDetails/UserDetails.tsx
+++ src/screens/UserDetails/UserDetails.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Container, Text } from './style';
+import { Company, Container, Name, PersonalData, Text, CompanyName, Address, SectionTitle, TextWrapper, LineIndicator } from './style';
 import { IUserRoute } from '../../globalInterfaces';
 
 type Props = {
@@ -13,7 +13,65 @@ function UserDetails({ route }: Props): React.ReactElement {
   
   return (
     <Container>
-      <Text>Meu componente</Text>
+      <PersonalData>
+        <SectionTitle>Dados pessoais:</SectionTitle>
+        <TextWrapper>
+          <Name>Nome - </Name>
+          <Name>{user.name}</Name>
+        </TextWrapper>
+        
+        <TextWrapper>
+          <LineIndicator>E-mail - </LineIndicator>
+          <Text>{user.email}</Text>
+        </TextWrapper>
+        
+        <TextWrapper>
+          <LineIndicator>Nick - </LineIndicator>
+          <Text>{user.username}</Text>
+        </TextWrapper>
+
+        <TextWrapper>
+          <LineIndicator>Phone - </LineIndicator>
+          <Text>{user.phone}</Text>
+        </TextWrapper>
+      </PersonalData>
+
+      <Company>
+        <SectionTitle>Dados da empresa:</SectionTitle>
+
+        <TextWrapper>
+          <CompanyName>Nome da empresa - </CompanyName>
+          <CompanyName>{user.company.name}</CompanyName>
+        </TextWrapper>
+
+        <TextWrapper>
+          <LineIndicator>Slogan - </LineIndicator>
+          <Text>{user.company.catchPhrase}</Text>
+        </TextWrapper>
+
+        <TextWrapper>
+          <LineIndicator>Bs - </LineIndicator>
+          <Text>{user.company.bs}</Text>
+        </TextWrapper>
+
+        <TextWrapper>
+          <LineIndicator>Site - </LineIndicator>
+          <Text>{user.website}</Text>
+        </TextWrapper>
+      </Company>
+
+      <Address>
+        <SectionTitle>Dados de endereço:</SectionTitle>
+        <TextWrapper>
+          <LineIndicator>Endereço - </LineIndicator>
+          <Text>{user.address.street}, {user.address.suite}, {user.address.city}</Text>
+        </TextWrapper>
+
+        <TextWrapper>
+          <LineIndicator>CEP - </LineIndicator>
+          <Text>{user.address.zipcode}</Text>
+        </TextWrapper>
+      </Address>
     </Container>
   );
 }
--- src/screens/UserDetails/style.ts
+++ src/screens/UserDetails/style.ts
@@ -1,12 +1,55 @@
 import styled from 'styled-components/native'
 
 export const Container = styled.View`
-  align-items: center;
-  justify-content: center
+  padding: 10px;
+  flex: 1;
+  background-color: #eeeeee;
 `;
 
+export const PersonalData = styled.View`
+  background-color: #ffffff;
+  padding: 10px;
+  border-radius: 5px;
+  margin-bottom: 15px;
+`
+
+export const Name = styled.Text`
+  font-weight: 500;
+  margin-bottom: 5px;
+`
+
 export const Text = styled.Text`
-  color: red;
+
+`;
+
+export const Company = styled.View`
+  background-color: #ffffff;
+  padding: 10px;
+  margin-bottom: 15px;
+  border-radius: 5px;
+` 
+
+export const CompanyName = styled.Text`
+  margin-bottom: 5px;
+  font-weight: 500;
+`
+
+export const Address = styled.View`
+  background-color: #ffffff;
+  padding: 10px;
+  border-radius: 5px;
+`
+
+export const SectionTitle = styled.Text`
   font-size: 16px;
   font-weight: bold;
-`;
+  margin-bottom: 5px;
+`
+
+export const TextWrapper = styled.View`
+  flex-direction: row;
+`
+
+export const LineIndicator = styled.Text`
+  font-weight: 500;
+`
